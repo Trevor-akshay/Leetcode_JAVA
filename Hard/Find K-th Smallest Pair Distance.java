442ca5415c204f5d3360476d40bd8e8c2b549e5f
AKSHAY <52254954+Trevor-akshay@users.noreply.github.com>
2020-11-04 19:56:13 +0530

    Add files via upload

---
+++ Hard/Find K-th Smallest Pair Distance.java
@@ -0,0 +1,25 @@
+class Solution {
+	public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+        int minK = 0, maxK = nums[nums.length - 1] - nums[0];
+        
+		
+        while (minK < maxK) {
+            int mid = (minK + maxK) / 2;
+            int count = 0, left = 0;
+            
+            
+            for (int right = 0; right < nums.length; ++right) {
+                while (true) {
+                    int diff = nums[right] - nums[left];
+                    if(diff > mid) left++;
+                    else break;
+                }
+              
+                count += right - left;
+            }
+            
+            if (count >= k) maxK = mid;
+            else minK = mid + 1;
+        }
+        return minK;}}
