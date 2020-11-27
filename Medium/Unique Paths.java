a6dafb98f7c0cb7ce6f75dc583598f3ff1d1e230
AKSHAY <52254954+Trevor-akshay@users.noreply.github.com>
2020-11-27 08:53:16 +0530

    Add files via upload

---
+++ Medium/Unique Paths.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+		for(int i = 0;i<m;i++)
+			dp[i][0] = 1;
+		for(int j = 1;j<n;j++)
+			dp[0][j] = 1;
+		for(int i = 1;i<m;i++){
+			for(int j = 1;j<n;j++)
+				dp[i][j] = dp[i][j-1] + dp[i-1][j];
+    }
+    //System.out.println(Arrays.deepToString(dp));
+    return dp[m-1][n-1];
+    }
+}
