d2d0899c3655b509542f09cadb324a5987921919
AKSHAY <52254954+Trevor-akshay@users.noreply.github.com>
2020-12-09 12:03:24 +0530

    Add files via upload

---
+++ Medium/Count Servers that Communicate.java
@@ -0,0 +1,25 @@
+class Solution {
+     public int countServers(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] rows = new int[m];
+        int[] cols = new int[n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) {
+                    ++rows[i];
+                    ++cols[j];
+                }
+            }
+        }
+        int cnt = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1 && (rows[i] > 1 || cols[j] > 1)) {
+                    ++cnt;
+                }
+            }
+        }
+        return cnt;
+    }
+}
