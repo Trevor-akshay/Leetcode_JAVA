2b5fd92d77aa201d848860613dbc0b47cb7ffbe7
AKSHAY <52254954+Trevor-akshay@users.noreply.github.com>
2020-12-16 20:18:01 +0530

    Update 395. Longest Substring with At Least K Repeating Characters.java

--- Medium/395. Longest Substring with At Least K Repeating Characters.java
+++ Medium/395. Longest Substring with At Least K Repeating Characters.java
@@ -1,10 +1,9 @@
 class Solution {
     public int longestSubstring(String s, int k) {
-         int freq[] = new int[26];
+        Map<Character,Integer> map = new HashMap();
         char[] str = s.toCharArray();
         for(char c: str){
-            int index =c -'a';
-            freq[index]++;
+            map.put(c,map.getOrDefault(c,0)+1);
         }
         
         boolean valid = true;
@@ -12,8 +11,7 @@ class Solution {
         int start =0;
         int maxLen = 0;
         for(int end = 0;end<s.length();end++) {
-            // TC -> O(N2)
-            if(freq[str[end] - 'a'] >0 && freq[str[end] - 'a'] <k ){
+            if(map.get(s.charAt(end)) > 0 && map.get(s.charAt(end)) < k){
                 String subString = s.substring(start, end);
                 maxLen  = Math.max(maxLen , longestSubstring(subString, k));
                 start = end+1;
