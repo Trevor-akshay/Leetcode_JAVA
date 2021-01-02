b56f7db3f0f63c4ab874e994551ba138eeedcf6a
AKSHAY <52254954+Trevor-akshay@users.noreply.github.com>
2021-01-02 14:02:53 +0530

    Add files via upload

---
+++ Easy/69. Sqrt(x).java
@@ -0,0 +1,16 @@
+class Solution {
+    public int mySqrt(int x) {
+        int left = 1, right = x;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid < x / mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+}
