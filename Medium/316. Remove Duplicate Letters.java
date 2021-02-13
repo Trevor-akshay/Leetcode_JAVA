f24cddc5f0af856dc57694acf25c2a01a1344966
AKSHAY <52254954+Trevor-akshay@users.noreply.github.com>
2021-02-13 10:52:16 +0530

    Add files via upload

---
+++ Medium/316. Remove Duplicate Letters.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+      Deque<Character> stack = new LinkedList<>();
+       Map<Character,Integer> freq = new HashMap<>();
+       for(char c : s.toCharArray())
+           freq.put(c,freq.getOrDefault(c,0)+1);
+
+       for(char c : s.toCharArray()){//abacb"
+           if(!stack.isEmpty() && stack.contains(c)) {
+               freq.put(c,freq.get(c)-1);
+               continue;
+           }
+           while (!stack.isEmpty() && c <= stack.peek()
+               && freq.get(stack.peek()) > 1){
+                   freq.put(stack.peek(),freq.get(stack.pop())-1);
+               }
+               stack.push(c);
+               }
+        System.out.println(stack);
+       StringBuilder stringBuilder = new StringBuilder();
+       while (!stack.isEmpty()){
+           stringBuilder.append(stack.pop());
+       }
+       return stringBuilder.reverse().toString();
+    }
+}
