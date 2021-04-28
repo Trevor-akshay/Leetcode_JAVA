808c4d7f4901734651d8d37e09b0a1b4c746715e
AKSHAY <52254954+Trevor-akshay@users.noreply.github.com>
2021-04-28 12:41:09 +0530

    Add files via upload

---
+++ Easy/303. Range Sum Query - Immutable.java
@@ -0,0 +1,24 @@
+class NumArray {
+    int[] prefixSum;
+    public NumArray(int[] nums) {
+        prefixSum = new int[nums.length];
+        prefixSum[0] = nums[0];
+        for(int i = 1;i<nums.length;i++)
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+    }
+    public int sumRange(int left, int right) {
+        if(left == 0)
+            return prefixSum[right];
+        return prefixSum[right] - prefixSum[left-1];
+    }
+    
+    private void calculateSum(){
+        
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
