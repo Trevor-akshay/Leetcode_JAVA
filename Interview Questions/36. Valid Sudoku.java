b11fa1d4dd1d8570f4db31657c1b3da9b6f69ba6
AKSHAY <52254954+Trevor-akshay@users.noreply.github.com>
2021-07-05 23:02:42 -0700

    Update 36. Valid Sudoku.java

--- Interview Questions/36. Valid Sudoku.java
+++ Interview Questions/36. Valid Sudoku.java
@@ -1,57 +1,31 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-         int row = 0;
-            int col = 0;
-            int startCol = 0;
-            int startRow = 0;
+        // row / 3 * 3 + col / 3 = square
+        Map<Integer,boolean[]> boxes = new HashMap<>();
+        Map<Integer,boolean[]> columns = new HashMap<>();
+        Map<Integer,boolean[]> rows = new HashMap<>();
 
-            int index = 0;
-            int charCount = 0;
-            char c = ' ';
-            while (index < 81) {
-                col = index % 9;
-                row = index / 9;
-                charCount = 0;
-                c = board[row][col];
-
-                if (c != '.') {
-                    for (int i = 0; i < 9; i++) { 
-                        if (board[row][i] == c) {
-                            if (charCount > 0) {
-                                return false;
-                            }
-                            charCount++;
-                        }
-                    }
-
-                    charCount = 0;
-                    for (int i = 0; i < 9; i++) { 
-                        if (board[i][col] == c) {
-                            if (charCount > 0) {
-                                return false;
-                            }
-                            charCount++;
-                        }
-                    }
-
-                    startCol = (col / 3) * 3;
-                    startRow = (row / 3) * 3;
-                    charCount = 0;
-                    for (int a = startCol; a < startCol + 3; a++) { 
-                        for (int b = startRow; b < startRow + 3; b++) {
-                            if (board[b][a] == c) {
-                                if (charCount > 0) {
-                                    return false;
-                                }
-                                charCount++;
-                            }
-                        }
-                    }
-                }
-
-                index++;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] =='.')
+                    continue;
+                int square = ((i / 3) * 3) + (j / 3);
+                int value = board[i][j] - '0';
+                
+                boxes.putIfAbsent(square, new boolean[10]);
+                rows.putIfAbsent(i, new boolean[10]);
+                columns.putIfAbsent(j, new boolean[10]);
+                
+                if (boxes.get(square)[value] ||
+                    rows.get(i)[value] ||
+                    columns.get(j)[value])
+                        return false;
+                
+                boxes.get(square)[value] = true;
+                rows.get(i)[value] = true;
+                columns.get(j)[value] = true;
             }
-
-            return true;
+        }
+        return true;
     }
 }
