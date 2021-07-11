53f585161afb2f22632b7125536dbfc4e3d717f3
AKSHAY <52254954+Trevor-akshay@users.noreply.github.com>
2021-07-11 07:09:10 -0700

    Delete Array/Easy directory

--- Array/Easy/Maximum Product of Three Numbers.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public int maximumProduct(int[] nums) {
-       Arrays.sort(nums);
-        int len = nums.length;
-        return Math.max(nums[0] * nums[1] * nums[len-1], nums[len-1] * nums[len-2] * nums[len-3]);
-    }
-}
