8b9c4713af341a5f3b22c9eb4f5fc1aabf1a83ef
AKSHAY <52254954+Trevor-akshay@users.noreply.github.com>
2021-07-25 08:45:20 -0700

    1306

---
+++ Graph/1306. Jump Game III.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        if(start < 0 || start >= arr.length || arr[start] < 0)
+            return false;
+
+         if(arr[start] == 0)
+            return true;
+        
+         arr[start] = -arr[start];
+        
+         return canReach(arr,start + arr[start]) || 
+                canReach(arr,start - arr[start]);
+    
+    }
+}
