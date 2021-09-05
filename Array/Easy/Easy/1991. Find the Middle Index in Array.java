e00f82d2167a9fabe8c68d682a0b5f042e8b3f49
AKSHAY <52254954+Trevor-akshay@users.noreply.github.com>
2021-09-04 23:29:24 -0700

    1991

---
+++ Array/Easy/Easy/1991. Find the Middle Index in Array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        
+        left[0] = nums[0];
+        for(int i = 1;i < n; i++){
+            left[i] = left[i - 1] + nums[i];
+        }
+        
+        right[n-1] = nums[n-1];
+        for(int i = n-2;i>=0;i--)
+            right[i] = right[i + 1] + nums[i];
+        
+        
+        for(int i = 0;i<n;i++){
+            if(left[i] - right[i] == 0)
+                return i;
+        }
+        
+        return -1;
+    }
+}
